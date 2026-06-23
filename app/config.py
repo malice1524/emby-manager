e10666cf7825832b2b0875f35a20789c1e7554bd
malice1524 <malice1524@users.noreply.github.com>
2026-06-23 18:35:22 +0800

    ✨ v1.08 完整修复所有未推送的修改

--- app/config.py
+++ app/config.py
@@ -3,3 +3,6 @@ import os
 EMBY_URL = os.getenv("EMBY_URL", "http://localhost:8096")
 EMBY_API_KEY = os.getenv("EMBY_API_KEY", "")
 HEADERS = {"X-Emby-Token": EMBY_API_KEY}
+
+EMBY_ADMIN_USER = os.getenv("EMBY_ADMIN_USER", "")
+EMBY_ADMIN_PW = os.getenv("EMBY_ADMIN_PW", "")
